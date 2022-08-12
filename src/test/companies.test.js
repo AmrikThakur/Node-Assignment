a1ed950430127459f72709b0ee8b3d210ef54ec1
Amrik Singh <amrikthakur1996@gmail.com>
2022-08-12 17:45:46 +0530

    finishes test cases

--- src/test/companies.test.js
+++ src/test/companies.test.js
@@ -6,25 +6,30 @@ const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 const next = jest.fn()
 
 
-test('check Get All Companies api give success status', async () => {
-    await getAllCompanies(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
-test('check Create Company api include email Id', async () => {
-    const { email } = req.body;
-    expect(email).not.toBe('')
-});
+describe('Testing Company API\'s', () => {
 
-test('check Create Company api give success status', async () => {
-    await createCompany(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
+    test('check Get All Companies api give success status', async () => {
+        await getAllCompanies(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
 
-test('check Update Company api give success status', async () => {
-    await updateCompany(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
-test('check Delete Company api give success status', async () => {
-    await deleteCompanyAPI(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
+    test('check Create Company api include email Id', async () => {
+        const { email } = req.body;
+        expect(email).not.toBe('')
+    });
+
+    test('check Create Company api give success status', async () => {
+        await createCompany(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
+
+    test('check Update Company api give success status', async () => {
+        await updateCompany(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
+    test('check Delete Company api give success status', async () => {
+        await deleteCompanyAPI(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
+
+})
--- src/test/employee.test.js
+++ src/test/employee.test.js
@@ -5,27 +5,30 @@ const req = { body: { emai: "amrikthakur1996@gmail.com" } }
 const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 const next = jest.fn()
 
+describe('Testing Employee API\'s', () => {
+    
+    test('check Get All Employee api give success status', async () => {
+        await getAllEmployee(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
 
-test('check Get All Employee api give success status', async () => {
-    await getAllEmployee(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
+    test('check Create Company api include email Id', async () => {
+        const { email } = req.body;
+        expect(email).not.toBe('')
+    });
 
-test('check Create Company api include email Id', async () => {
-    const { email } = req.body;
-    expect(email).not.toBe('')
-});
+    test('check Create Employee api give success status', async () => {
+        await createEmployee(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
 
-test('check Create Employee api give success status', async () => {
-    await createEmployee(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
+    test('check Update Employee api give success status', async () => {
+        await updateEmployee(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
+    test('check Delete Employee api give success status', async () => {
+        await deleteEmployee(req, res, next);
+        expect(res.status).toBeCalledWith(200);
+    });
 
-test('check Update Employee api give success status', async () => {
-    await updateEmployee(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
-test('check Delete Employee api give success status', async () => {
-    await deleteEmployee(req, res, next);
-    expect(res.status).toBeCalledWith(200);
-});
+})

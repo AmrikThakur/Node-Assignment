fc9aba8c9f5e3f698e4d48fabd69eb61c12ba0d1
Amrik Singh <amrikthakur1996@gmail.com>
2022-08-13 18:20:09 +0530

    middleware for non existing routes

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,7 +1,13 @@
 const companyRoutes = require('../routes/companies')
 const employeRoutes = require('../routes/employee')
+const AppError = require('../common/appError');
 
 module.exports = function (app) {
     app.use('/api/v1/company', companyRoutes)
     app.use('/api/v1/employee', employeRoutes)
+
+    // Error middleware for non-existent routes 
+    app.all('*', (req, res, next) => {
+        return next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+    });
 }
